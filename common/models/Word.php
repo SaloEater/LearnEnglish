68834d3b242b4f40e592f1b2f479047877e1930d
rusl96 <izmaylov.rusl@yandex.ru>
2019-07-14 00:03:02 +0300

    добавлен http-client, создан метод lemma модели word

--- common/models/Word.php
+++ common/models/Word.php
@@ -3,6 +3,7 @@
 namespace common\models;
 
 use Yii;
+use yii\httpclient\Client;
 
 /**
  * This is the model class for table "word".
@@ -70,5 +71,31 @@ class Word extends \yii\db\ActiveRecord
     public function getSentences()
     {
         return $this->hasMany(Sentence::className(), ['id' => 'sentence_id'])->viaTable('sentences_words', ['word_id' => 'id']);
+    }
+
+    function lemma($word)
+    {
+        $client = new Client();
+        $response = $client->createRequest()
+            ->setMethod('POST')
+            ->setUrl('http://paraphraser.ru/api')
+            ->setData([
+                'token' => '85f2fbd3f9fef2e1b4595f451490dbc010de50d1',
+                'c' => 'vector',
+                'query' => $word,
+                'top' => 1,
+                'lang' => 'en',
+                'forms' => 0,
+                'scores' => 0,
+                'format' => 'json'
+            ])
+            ->send();
+        if ($response->isOk) {
+            $data = $response->data;
+            $lemma = $data['response'][1]['lemma'];
+            if ($word == $lemma){
+                return true;
+            } else return $lemma;
+        } else return false;
     }
 }
