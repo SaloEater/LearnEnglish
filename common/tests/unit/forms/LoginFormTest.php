ceb6bd48146b6ac2e20d9cc1c97ceb22b2438ca8
SaloEater <danil.lazarev@gmail.com>
2019-07-17 18:42:54 +0300

    AssertThrows

--- common/tests/unit/forms/LoginFormTest.php
+++ common/tests/unit/forms/LoginFormTest.php
@@ -1,9 +1,11 @@
 <?php
 
-namespace common\tests\unit\models;
-
+namespace common\tests\unit\forms;
+use Codeception\AssertThrows;
+use PHPUnit\Framework\AssertionFailedError;
 use common\fixtures\UserFixture;
 use common\forms\LoginForm;
+use common\services\AuthService;
 use Yii;
 
 /**
@@ -11,6 +13,8 @@ use Yii;
  */
 class LoginFormTest extends \Codeception\Test\Unit
 {
+    use AssertThrows;
+
     /**
      * @var \common\tests\UnitTester
      */
@@ -32,36 +36,40 @@ class LoginFormTest extends \Codeception\Test\Unit
 
     public function testLoginNoUser()
     {
-        $model = new LoginForm([
+        $form = new LoginForm([
             'username' => 'not_existing_username',
             'password' => 'not_existing_password',
         ]);
 
-        expect('model should not login user', $model->login())->false();
+        $this->assertThrows(\DomainException::class, function() use ($form){
+            (new AuthService())->auth($form);
+        });
         expect('user should not be logged in', Yii::$app->user->isGuest)->true();
     }
 
     public function testLoginWrongPassword()
     {
-        $model = new LoginForm([
+        $form = new LoginForm([
             'username' => 'bayer.hudson',
             'password' => 'wrong_password',
         ]);
 
-        expect('model should not login user', $model->login())->false();
-        expect('error message should be set', $model->errors)->hasKey('password');
+        $this->assertThrows(\DomainException::class, function() use ($form){
+            (new AuthService())->auth($form);
+        });
+        expect('error message should be set', $form->errors)->hasKey('password');
         expect('user should not be logged in', Yii::$app->user->isGuest)->true();
     }
 
     public function testLoginCorrect()
     {
-        $model = new LoginForm([
+        $form = new LoginForm([
             'username' => 'bayer.hudson',
             'password' => 'password_0',
         ]);
 
-        expect('model should login user', $model->login())->true();
-        expect('error message should not be set', $model->errors)->hasntKey('password');
+        $user = (new AuthService())->auth($form);
+        expect('error message should not be set', $form->errors)->hasntKey('password');
         expect('user should be logged in', Yii::$app->user->isGuest)->false();
     }
 }
