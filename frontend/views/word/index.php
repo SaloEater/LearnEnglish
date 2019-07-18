e1f180913939a81289a00ac7a276bc8573153ad5
SaloEater <danil.lazarev@gmail.com>
2019-07-18 18:56:19 +0300

    Фильтр по связи

--- frontend/views/word/index.php
+++ frontend/views/word/index.php
@@ -1,6 +1,7 @@
 <?php
 
 /* @var $dataProvider  ActiveDataProvider*/
+/* @var $filterModel \backend\models\UsersWordsSearch*/
 use yii\data\ActiveDataProvider;
 use common\entities\UsersWords;
 use yii\helpers\ArrayHelper;
@@ -8,35 +9,44 @@ $models = $dataProvider->getModels();
 
 $countMap = ArrayHelper::map($models, 'count', 'count');
 
+$globalCountMap = ArrayHelper::map($models, 'count', 'word.count');
+
 ?>
 
 <?= \yii\grid\GridView::widget([
     'dataProvider' => $dataProvider,
+    'filterModel' => $filterModel,
     'columns' => [
         [
-            'attribute' => 'Слово',
+            'label' => 'Слово',
             'value' => function (UsersWords $data) {
                 return $data->word->content;
             },
+//            'filter' => false
         ],
         [
-            'attribute' => 'Статус',
+            'attribute' => 'status',
             'value' => function (UsersWords $data) {
                 return $data->status;
             },
+            'filter' => [
+                '0' => 'Не изучено',
+                '1' => 'Изучено',
+            ]
         ],
         [
-            'attribute' => 'У вас',
+            'label' => 'У вас',
+            'attribute' => 'count',
             'value' => function (UsersWords $data) {
                 return $data->count;
             },
             'filter' => $countMap
         ],
         [
-            'attribute' => 'Глобально',
-            'value' => function (UsersWords $data) {
-                return $data->word->count;
-            },
-        ],
+            'attribute' => 'word',
+            'value' => 'word.count',
+            'label' => 'Глобально',
+            'filter' => $globalCountMap
+        ]
     ],
 ]) ?>
